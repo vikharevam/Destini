b23a19e5328b12848cadd63d4d2bb2a8122ff7b3
vikharevam <103068846+vikharevam@users.noreply.github.com>
2023-02-09 17:10:05 +0300

    добавлена функция обновления UI

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -8,21 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var qwestDialog: UILabel!
-    
-    
     @IBOutlet weak var optionOne: UIButton!
-    
     @IBOutlet weak var optionTwo: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
         // Do any additional setup after loading the view.
+        
+        
+        func updateUI() {
+            qwestDialog.text = storyBrain.getStoriesTitle()
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
     }
-
-
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
---
+++ Destini/Story.swift
@@ -0,0 +1,27 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Александр Вихарев on 09.02.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination2: Int
+    
+}
+
+        
+        
+        
+        
+
+    
+    
+
+
