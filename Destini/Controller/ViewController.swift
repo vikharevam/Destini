34f3c9103b8596b17f1241b62160e65103f1640e
vikharevam <103068846+vikharevam@users.noreply.github.com>
2023-02-09 15:00:44 +0300

    Outlet

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var qwestDialog: UILabel!
+    
+    
+    @IBOutlet weak var optionOne: UIButton!
+    
+    @IBOutlet weak var optionTwo: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
